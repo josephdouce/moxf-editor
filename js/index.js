d07e7c333b6a6b4dc0d21b53488679caeca93796
joseph douce <josephdouce@gmail.com>
2019-02-20 22:57:23 +0000

    sysex

--- js/index.js
+++ js/index.js
@@ -15,6 +15,31 @@ WebMidi.enable(function (err) {
                 document.getElementById("demo").innerHTML = e.note.name + e.note.octave; 
             }
         );
+
+        // Listen to pitch bend message on channel all channels
+        input.addListener('pitchbend', 'all',
+            function (e) {
+                console.log("Received 'pitchbend' message.", e);
+                document.getElementById("demo").innerHTML = e;
+            }
+        );
+
+        // Listen to control change message on all channels
+        input.addListener('controlchange', "all",
+            function (e) {
+                console.log("Received 'controlchange' message.", e);
+                document.getElementById("demo").innerHTML = e;
+            }
+        );
+
+        // Listen to csysex message on all channels
+        input.addListener('sysex', "all",
+            function (e) {
+                console.log("Received 'sysex' message.", e);
+                document.getElementById("demo").innerHTML = e;
+            }
+        );
+
     }
 }, true);
 
