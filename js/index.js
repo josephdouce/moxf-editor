65d31cda187fe28b5fbc8d6511fbd556e30b0297
joseph douce <josephdouce@gmail.com>
2019-02-21 11:19:40 +0000

    tabs midi i/o moved to midi page

--- js/index.js
+++ js/index.js
@@ -117,3 +117,18 @@ function getMidiDevices() {
         }
     }, true);
 }
+
+function openTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tabPage");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+
+function onLoadFunction() {
+    getMidiDevices();
+}
+
+window.onload = onLoadFunction();
