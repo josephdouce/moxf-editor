11d2f38418bcfabcfc5183459e8f643dffdc1f1c
josephdouce <josephdouce@gmail.com>
2019-03-11 17:40:50 +0000

    WebApp

--- js/main.js
+++ js/main.js
@@ -276,6 +276,8 @@ window.addEventListener('beforeinstallprompt', function (e) {
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
+  
+  console.log(e);
 
   showAddToHomeScreen();
 
@@ -291,18 +293,20 @@ function showAddToHomeScreen() {
 
 }
 
-function addToHomeScreen() {  var a2hsBtn = document.querySelector(".ad2hs-prompt");  // hide our user interface that shows our A2HS button
-  a2hsBtn.style.display = 'none';  // Show the prompt
-  deferredPrompt.prompt();  // Wait for the user to respond to the prompt
+function addToHomeScreen() {
+  var a2hsBtn = document.querySelector(".ad2hs-prompt"); // hide our user interface that shows our A2HS button
+  a2hsBtn.style.display = 'none'; // Show the prompt
+  deferredPrompt.prompt(); // Wait for the user to respond to the prompt
   deferredPrompt.userChoice
-    .then(function(choiceResult){
+    .then(function (choiceResult) {
 
-  if (choiceResult.outcome === 'accepted') {
-    console.log('User accepted the A2HS prompt');
-  } else {
-    console.log('User dismissed the A2HS prompt');
-  }
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
 
-  deferredPrompt = null;
+      deferredPrompt = null;
 
-});}
+    });
+}
