d8f089afa68dedb7c1efbf03464757b104d4a430
josephdouce <josephdouce@gmail.com>
2019-03-13 18:10:33 +0000

    cache-first

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,61 +1,74 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+// This is the service worker with the Cache-first network
 
-const CACHE = "pwabuilder-offline-page";
+const CACHE = "pwabuilder-precache";
+const precacheFiles = [
+  /* Add an array of files to precache for your app */
+];
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
-
-// Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
   console.log("[PWA Builder] Install Event processing");
 
+  console.log("[PWA Builder] Skip waiting on install");
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Cached offline page during install");
-      
-      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
-        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-      }
-      
-      return cache.add(offlineFallbackPage);
+      console.log("[PWA Builder] Caching pages during install");
+      return cache.addAll(precacheFiles);
     })
   );
 });
 
+// Allow sw to control of current page
+self.addEventListener("activate", function (event) {
+  console.log("[PWA Builder] Claiming clients for current page");
+  event.waitUntil(self.clients.claim());
+});
+
 // If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", function (event) { 
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then(function (response) {
-        console.log("[PWA Builder] add page to offline cache: " + response.url);
+    fromCache(event.request).then(
+      function (response) {
+        // The response was found in the cache so we responde with it and update the entry
 
-        // If request was success, add or update it in the cache
-        event.waitUntil(updateCache(event.request, response.clone()));
+        // This is where we call the server to get the newest version of the
+        // file to use the next time we show view
+        event.waitUntil(
+          fetch(event.request).then(function (response) {
+            return updateCache(event.request, response);
+          })
+        );
 
         return response;
-      })
-      .catch(function (error) {
-        console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
-        return fromCache(event.request);
-      })
+      },
+      function () {
+        // The response was not found in the cache so we look for it on the server
+        return fetch(event.request)
+          .then(function (response) {
+            // If request was success, add or update it in the cache
+            event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function (error) {
+            console.log("[PWA Builder] Network request failed and no cache." + error);
+          });
+      }
+    )
   );
 });
 
 function fromCache(request) {
   // Check to see if you have it in the cache
   // Return response
-  // If not in the cache, then return the offline page
+  // If not in the cache, then return
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
       if (!matching || matching.status === 404) {
-        // The following validates that the request was for a navigation to a new document
-        if (request.destination !== "document" || request.mode !== "navigate") {
-          return Promise.reject("no-match");
-        }
-
-        return cache.match(offlineFallbackPage);
+        return Promise.reject("no-match");
       }
 
       return matching;
