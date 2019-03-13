8a2456d2b6c69004d73eb8ecc4e67f020b65e8f5
josephdouce <josephdouce@gmail.com>
2019-03-13 18:11:21 +0000

    cache-first

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -26,7 +26,7 @@ self.addEventListener("activate", function (event) {
 });
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) { 
+self.addEventListener("fetch", function (event) {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
